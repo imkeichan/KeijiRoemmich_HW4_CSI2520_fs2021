9874807b27b81688fa72f5767f24f27bf5acebcd
Keiji Roemmich <keiji0627@live.com>
2021-11-14 23:02:54 -0500

    final commit for now

--- js/plugin.js
+++ js/plugin.js
@@ -1,13 +1,4 @@
-$(document).ready(function(){ 
- 
-    $(".primaryContained").on('click', function(){
-    $(".comment").addClass("commentClicked");
-  });//end click
-  $("textarea").on('keyup.enter', function(){
-    $(".comment").addClass("commentClicked");
-  });//end keyup
-  });//End Function
- 
+
 new Vue({
     el: "#app",
     data:{
